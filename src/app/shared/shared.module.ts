74f7423f14eaf4f52ea95a27be7a56963abcf7b8
Cam Remesz <saltyndead@gmail.com>
2023-05-06 16:49:43 -0400

    Build reusable input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
 
 
 
@@ -10,11 +13,15 @@ import { TabComponent } from './tab/tab.component';
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
+  providers: [provideEnvironmentNgxMask()]
 })
 export class SharedModule { }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,36 +8,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  name = new FormControl('', [Validators.minLength(2), Validators.required])
+  email = new FormControl('', [Validators.required, Validators.email])
+  age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)])
+  password = new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)])
+  confirmPassword = new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)])
+  tel = new FormControl('')
+
   registerForm = new FormGroup({
-    name: new FormControl('', [Validators.minLength(2), Validators.required]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    tel: new FormControl(''),
-  })
-
-  get name() {
-    return this.registerForm.get('name');
-  }
-
-  get email() {
-    return this.registerForm.get('email');
-  }
-
-  get age() {
-    return this.registerForm.get('age');
-  }
-
-  get password() {
-    return this.registerForm.get('password');
-  }
-
-  get confirmPassword() {
-    return this.registerForm.get('confirmPassword');
-  }
-
-  get tel() {
-    return this.registerForm.get('tel');
-  }
+   name: this.name,
+   email: this.email,
+   age: this.age,
+   password: this.password,
+   confirmPassword: this.confirmPassword,
+   tel: this.tel
+  }) 
 }
