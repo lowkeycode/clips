6e7cdf692121b111900637f95a7cafc629556db6
Cam Remesz <saltyndead@gmail.com>
2023-05-07 18:11:29 -0400

    Add template form to login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  credentials = {
+    email: '',
+    password: ''
+  }
+
+  login(event: Event) {
+    console.log(this.credentials);
+    console.log(event);
+    
+    
+  }
+
 }
